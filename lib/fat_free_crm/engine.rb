6757d7bc8b5bfabc5e5a74c9416c8046d720b5e6
Ben Tillman <ben.tillman@gmail.com>
2012-02-09 11:27:49 +1300

    Passing all specs except observer related

--- lib/fat_free_crm/engine.rb
+++ lib/fat_free_crm/engine.rb
@@ -1,7 +1,7 @@
 module FatFreeCRM
   class Engine < Rails::Engine
     config.autoload_paths += Dir[root.join("app/models/**")]
-    
+
     config.to_prepare do
       %w(is_paranoid country_select dynamic_form gravatar_image_tag responds_to_parent).each do |plugin|
         $:.unshift File.join(File.dirname(__FILE__), '..', plugin, 'lib')
